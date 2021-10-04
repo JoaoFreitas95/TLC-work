efdad8e340c38c22ff0b6c7e1960ff6f10adc363
JoaoFreitas95 <33905973+JoaoFreitas95@users.noreply.github.com>
2021-10-04 21:24:32 +0100

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Parse_Tree_textBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(550, 467);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Parse_Tree_textBox
+            // 
+            this.Parse_Tree_textBox.Location = new System.Drawing.Point(782, 27);
+            this.Parse_Tree_textBox.Margin = new System.Windows.Forms.Padding(4);
+            this.Parse_Tree_textBox.Multiline = true;
+            this.Parse_Tree_textBox.Name = "Parse_Tree_textBox";
+            this.Parse_Tree_textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Parse_Tree_textBox.Size = new System.Drawing.Size(424, 468);
+            this.Parse_Tree_textBox.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(455, 142);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(306, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(550, 102);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(211, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(455, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(550, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Load";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(422, 467);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1219, 554);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Parse_Tree_textBox);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Parse_Tree_textBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using com.calitha.commons;
+using System.IO;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        MyParser MyParser = new MyParser();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Button "Check"
+            button1.Enabled = false;
+
+            if (MyParser.Parse(new StringReader(richTextBox1.Text)))
+            {
+                DrawReductionTree(MyParser.Root);
+            }
+            else
+            {
+                Parse_Tree_textBox.Text = MyParser.FailMessage;
+            }
+
+            button1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Loads EGT enabeling action => "Browse" and "Check"
+            MyParser.Setup(Application.StartupPath + "\\project 1.egt");
+            button3.Enabled = false;
+        }
+
+        private void DrawReductionTree(GOLD.Reduction Root)
+        {
+            //This procedure starts the recursion that draws the parse tree.
+            StringBuilder tree = new StringBuilder();
+
+            tree.AppendLine("+-" + Root.Parent.Text(false));
+            DrawReduction(tree, Root, 1);
+
+            Parse_Tree_textBox.Text = tree.ToString();
+        }
+
+        private void DrawReduction(StringBuilder tree, GOLD.Reduction reduction, int indent)
+        {
+            //Recursive procedure that draws an ASCII version of the parse
+            //Tree
+            int n;
+            string indentText = "";
+
+            for (n = 1; n <= indent; n++)
+            {
+                indentText += "| ";
+            }
+
+            //Displays the children of the reduction
+            for (n = 0; n < reduction.Count(); n++)
+            {
+                switch (reduction[n].Type())
+                {
+                    case GOLD.SymbolType.Nonterminal:
+                        GOLD.Reduction branch = (GOLD.Reduction)reduction[n].Data;
+
+                        tree.AppendLine(indentText + "+-" + branch.Parent.Text(false));
+                        DrawReduction(tree, branch, indent + 1);
+                        break;
+
+                    default:
+                        string leaf = (string)reduction[n].Data;
+
+                        tree.AppendLine(indentText + "+-" + leaf);
+                        break;
+                }
+            }
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();//Enables file browse and select
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Button "Browse"
+            ofd.Filter = "TXT|*.txt";//Filter to only show type=txt
+            ofd.ShowDialog();//Shows file modally
+            textBox2.Text = ofd.FileName;//Save file name
+            textBox3.Text = ofd.SafeFileName;//Save file location
+            if (textBox2.Text != null) { button3.Enabled = true; }//Enables file load
+        }
+
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Button "Load"
+            Stream myStream;//Enables read file
+
+                if((myStream = ofd.OpenFile()) != null)
+                {
+                    string strfilename = textBox2.Text;//File name
+                    string fileText = File.ReadAllText(strfilename);//Reads file text
+                    richTextBox1.Text = fileText;//Displays file text
+                }
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
